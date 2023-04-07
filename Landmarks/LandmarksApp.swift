cbb0c13a89d9f316f3195bd8c746de0394c946b1
Stanislav Derpoliuk <derpoliuk@gmail.com>
2023-04-06 21:02:39 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stanislav Derpoliuk on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
